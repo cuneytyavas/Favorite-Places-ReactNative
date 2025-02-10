83a9c84c5f516ea032b3147fc3d906511cbf8dc3
Cüneyt Yavaş <cuneytyavass@outlook.com>
2025-02-10 18:24:29 +0300

    Update location.js

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBPFXq7iaUOEb1jQ6d2fkiGlw43kHqEK_E";
+const GOOGLE_API_KEY = "Enter Your Google API Key Here.";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
